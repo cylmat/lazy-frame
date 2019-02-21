569218b71cc0c1d30a3d9aaa11b638acd1a17a1e
freeonisland <freeonisland@yahoo.fr>
2019-02-21 10:21:26 +0100

    composer

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,26 @@ var gulp        = require('gulp'),
     browserSync = require('browser-sync').create(),
     reload      = browserSync.reload;
 
-var htmlWatch = '**/*.html';
-var phpWatch = '**/*.php';
+var htmlWatch  = '**/*.html',
+    phpWatch   = '**/*.php',
+    cssWatch   = '**/*.css',
+    jsWatch   = '**/*.js',
+    sassWatch  = '**/*.sass',
+    cofeeWatch = '**/*.cofee';
+    
+var jsSrc = 'src/js/',
+    jsDist = 'dist/js/';
 
-/*gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function() {
     browserSync.init({
-        proxy: "http://wordpress:8888"
+        injectChanges: true,
+        watch: true,
+        files: "**/*",
+        proxy: "http://wordpress:8888",
+        host: "wordpress",
+        browser: "firefox"
     });
-});*/
+});
 
 gulp.task('serve', function() {
     browserSync.init({
@@ -20,12 +32,13 @@ gulp.task('serve', function() {
         host: "wordpress",
         browser: "firefox"
     });
+    
     //gulp.watch( htmlWatch, reload );
     //gulp.watch( phpWatch, reload );
-    gulp.watch( "**/*.php" ).on("change", browserSync.reload);
-    gulp.watch( "**/*.css" ).on("change", browserSync.reload);
-    gulp.watch( "**/*.html" ).on("change", browserSync.reload);
-    gulp.watch( "**/*.js" ).on("change", browserSync.reload);
+    gulp.watch( [htmlWatch, phpWatch, cssWatch] ).on("change", browserSync.reload);
+    //gulp.watch( "**/*.css" ).on("change", browserSync.reload);
+    //gulp.watch( "**/*.html" ).on("change", browserSync.reload);
+    //gulp.watch( "**/*.js" ).on("change", browserSync.reload);
 });
 
 gulp.task('default', gulp.series('serve'));
