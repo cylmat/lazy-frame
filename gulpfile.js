d44acbd296524fe73e24f7e7c2cc3079c2bc411d
freeonisland <freeonisland@yahoo.fr>
2019-03-03 14:53:07 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,51 @@
 /**
  * REQUIRES
  */
-var gulp          = require('gulp'),
-    gulp_less     = require('gulp-less'),
-    less          = require('less');
-    browserSync   = require('browser-sync').create(),
-    child_process = require("child_process").exec,
+var gulp         = require('gulp'),
+    browserSync  = require('browser-sync').create(),
+    childProcess = require("child_process").exec,
+    glupLoadPlugins  = require('gulp-load-plugins');
     
-    reload        = browserSync.reload;
+var glp = glupLoadPlugins();
     
 /**
 * WATCHES
 */
 var src  = './src/',
+    build= './build', 
     dist = './dist/';
 
 
-var htmlWatch  = '**/*.html',
-    phpWatch   = '**/*.php',
+var htmlWatch  = src+'**/*.html',
+    lessWatch  = src+'less/*.less',
+    sassWatch  = src+'sass/*.sass',
+    coffeeWatch= src+'coffee/*.coffee';    
+        
+    phpWatch   = './app/**/*.php',
     phpIndexWatch = 'index.php',
-    cssWatch   = '**/*.css',
-    lessWatch  = src + '**/*.less',
-    jsWatch    = '**/*.js',
-    sassWatch  = '**/*.sass',
-    xmlWatch   = '**/*.xml',
-    coffeeWatch = '**/*.coffee';
+    cssWatch   = src + '**/*.css',
+    jsWatch    = src + '**/*.js';
 
-var cssSrc  = src+'css/',
-    cssDist = dist+'css/';
-
-var jsSrc  = src+'js/',
-    jsDist = dist+'js/';
+var cssDist = dist+'css/',
+    jsDist  = dist+'js/';
   
 /**
  * Less
  */
-gulp.task('compile-gulp-less', function() {  
-    return gulp.src( './src/**/*.less' )
-        .pipe( gulp_less( ) )
-        .pipe( gulp.dest( './dist/css' ) );
+gulp.task('less', function() {  
+    gulp.src( lessWatch )
+        .pipe( glp.less( ) )
+        .pipe( gulp.dest( cssBuild ) )
+        .pipe( browserSync.reload() );
 }); 
 
-gulp.task('compile-less', function() {  
-    return gulp.src( './src/**/*.css' )
-        .pipe( less( ) )
-        .pipe( gulp.dest( './dist/css' ) );
-}); 
+gulp.task('minify', function(){
+    gulp.src('');
+});
 
 /**
  * Php-unit
- */
+ *
 gulp.task('php-unit', function() {
     child_process("phpunit -c ./config/phpunit.xml", function(error, stdout, stderr) {
         console.log('error'+error);
@@ -60,7 +56,7 @@ gulp.task('php-unit', function() {
 
 /**
  * Php-cs
- */
+ *
 gulp.task('php-cs', function() {
     //child_process('phpcs -s ./application', function(error,stdout) {
         child_process('phpcs ./application --ignore=./application/log/* --report=xml --report-file=./var/logphpcs.xml', function(error,stdout) {
@@ -72,45 +68,45 @@ gulp.task('php-cs', function() {
 
 /**
  * php-md
- */
+ *
 gulp.task('php-md', function() {
     child_process('phpmd ./application xml ./config/phpmd.xml --reportfile ./var/logphpmd.xml', function(error, stdout) {
         console.log('error:'+error);
         console.log('stdout:'+stdout);
     });
-});
+});*/
 
 
 
-/*gulp.task('browser-sync', function() {
-    browserSync.init({
+gulp.task('browser_sync', function() {
+    browserSync({
         injectChanges: true,
-        watch: true,
         files: "** /*",
         proxy: "http://wordpress:8888",
         host: "wordpress",
         browser: "firefox"
     });
-});*/
+});
 
 gulp.task('serve', function() {
     
-    browserSync.init({
+    /*browserSync.init({
         injectChanges: true,
         //watchEvents : [ 'change', 'add', 'unlink', 'addDir', 'unlinkDir' ],
         watch: true,
-        files: "**/*",
+        files: "** /*",
         proxy: "http://wordpress:8888",
         host: "wordpress",
         browser: "firefox"
-    });
+    });*/
+    browser_sync.reload();
     
     gulp.watch( [htmlWatch, cssWatch, phpWatch, jsWatch] ).on( "change", browserSync.reload );
-    gulp.watch( [lessWatch] ).on( "change", gulp.series('compile-less'));
+    gulp.watch( [lessWatch] ).on( "change", gulp.series('less'));
     
-    gulp.watch( phpIndexWatch ).on( "change", gulp.series('php-unit') );
-    gulp.watch( phpIndexWatch ).on( 'change', gulp.series('php-cs') );
-    gulp.watch( phpIndexWatch ).on( 'change', gulp.series('php-md') );
+    //gulp.watch( phpIndexWatch ).on( "change", gulp.series('php-unit') );
+    //gulp.watch( phpIndexWatch ).on( 'change', gulp.series('php-cs') );
+    //gulp.watch( phpIndexWatch ).on( 'change', gulp.series('php-md') );
 
 });
 
