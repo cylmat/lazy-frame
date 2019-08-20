68f8ca3589c4d9c350483a871ec9bac25071f7db
freeonisland <freeonisland@yahoo.fr>
2019-08-20 16:39:34 +0200

    layout

--- app/view/app/default/index.html.php
+++ app/view/app/default/index.html.php
@@ -1,3 +1,18 @@
- l'inddex
-
-<?= 'r' ?>coin
+<p class="accueil">
+    Bienvenue sur votre page d'accueil
+</p>
+<p class="loaded">
+    <h3>Composants chargés</h3>
+    <ul>
+        <li>HttpRequest</li>
+        <li>HttpResponse</li>
+        <li>Controller</li>
+    </ul>
+</p>
+<p class="dispo">
+    <h3>Composants disponibles</h3>
+    <ul>
+        <li>Repository</li>
+        <li>Entity</li>
+    </ul>
+</p>
--- app/view/app/layout.html.php
+++ app/view/app/layout.html.php
@@ -1,12 +1,20 @@
-<html>
+<!doctype html>
+<html lang="fr">>
     <head>
-        <title>Voici</title>
+        <title>Lazy-Frame framework</title>
+        <link rel="stylesheet" type="text/css" href="public/assets/css/style.css"/>
     </head>
     <body>
-        <h1>Bonfjour a tous</h1>
-
-        <?= $content ?>
-
-        <div>...et merci à tous</div>
+        <header>
+            <h1>Lazy - Frame</h1>
+        </header>
+        <main>
+            <?= $content ?>
+        </main>
+        <footer>
+            <div>Temps de chargement : 1.3ms</div>
+            <div>Module [App] - Controller [Default] - Action [Index]</div>
+            <div>Mémoire utilisée: 56ko</div>
+        </footer>
     </body>
 </html>
